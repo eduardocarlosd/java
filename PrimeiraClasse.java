15f29083627e31558df5b5f58b7bbdae7866ea80
Carlos Eduardo <eduardocarlosds1@gmail.com>
2023-08-18 00:18:40 -0300

    codigo java

---
+++ PrimeiraClasse.java
@@ -0,0 +1,13 @@
+public class PrimeiraClasse {
+    public static void main (String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Harry Potter");
+
+        int anoDeLancamento = 2023;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluindoPlano = true;
+        System.out.println("O plano é: " + incluindoPlano);
+        double notaDoFilme = 9.9;
+        System.out.println("Nota do filme: " + notaDoFilme);
+    }
+}
