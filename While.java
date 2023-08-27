fad96bdebb330a7c8f2fc63d285b03d9b2b9587e
Carlos Eduardo <eduardocarlosds1@gmail.com>
2023-08-26 22:15:01 -0300

    while

--- While.java
+++ While.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class While {
-}
+import java.util.Scanner;
+
+public class While {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        double mediaAvaliaca = 0;
+        double nota = 0;
+        int totalDeNotas = 0;
+
+        while (nota != -1) {
+            System.out.println("Diga sua avaliação para o filme ou -1 para encerrar: ");
+            nota = leitura.nextDouble();
+
+            if (nota != -1) {
+                mediaAvaliaca += nota;
+                totalDeNotas++;
+            }
+        }
+            System.out.println("Média de avaliações: " + mediaAvaliaca / totalDeNotas + " estrelas");
+        }
+    }
+
